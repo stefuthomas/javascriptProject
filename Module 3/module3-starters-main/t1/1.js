dbb2945bf6c512bbd5fb92e4dab43cc5aeb05933
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2023-11-13 14:15:37 +0200

    initial commit

---
+++ Module 3/module3-starters-main/t1/1.js
@@ -0,0 +1,3 @@
+'use strict';
+document.getElementById('target').classList.add('my-list');
+document.getElementById('target').innerHTML = '<li>First item</li>' + '<li>Second item</li>' + '<li>Third item</li>';
