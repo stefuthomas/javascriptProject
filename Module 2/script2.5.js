63368b24c28ee7cef2887a08d9d438761fd634c6
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2023-11-15 14:36:51 +0200

    commit

--- Module 2/script2.5.js
+++ Module 2/script2.5.js
@@ -6,6 +6,7 @@ while (countingNumbers) {
     let num = parseInt(prompt('Enter a number'));
     numbers.push(num);
     if (num in numbers) {
+        alert(`number ${num} has already been given`)
         break
     }
 }
